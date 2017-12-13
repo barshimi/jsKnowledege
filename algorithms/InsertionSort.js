a89719f2a6896fe6b99d47536791b8cf24cfad1a
Shimi Bar <shimi.b@taboola.com>
2017-12-13 13:28:49 +0200

    initial

---
+++ algorithms/InsertionSort.js
@@ -0,0 +1,25 @@
+'use strict'
+
+/**
+ * Insertion sort - O(n²)
+ * @param  {[type]} array [description]
+ * @return {[type]}       [description]
+ */
+function insertionSort(array) {
+  var length = array.length;
+
+  for(var i = 1; i < length; i++) {
+    var temp = array[i];
+    console.log('i', temp);
+    for(var j = i - 1; j >= 0 && array[j] > temp; j--) {
+      array[j+1] = array[j];
+      console.log('j', array[j], array);
+    }
+    array[j+1] = temp;
+  }
+
+  return array;
+}
+
+
+console.log(insertionSort([98, 65, 34, 1, 20, 0]));
